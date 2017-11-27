e26733bde95bcab0f3d2ab9ea124df5f81e3965f
Duy Nguyen <duyso1000@gmail.com>
2017-11-27 13:26:19 +0700

    Add newsfeed

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,7 +4,7 @@ class ReviewsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
 
   def index
-    @reviews = Review.most_recent.paginate(page: params[:page], per_page: 5)
+    @reviews = Review.most_recent
   end
 
   def new
